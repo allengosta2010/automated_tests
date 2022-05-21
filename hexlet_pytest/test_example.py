cc663107b04fd68618ce8dea8d0e611584433b7c
allengosta2010 <www.allengosta@gmail.com>
2022-05-21 12:47:22 +0300

    test: new tests

---
+++ hexlet_pytest/test_example.py
@@ -0,0 +1,8 @@
+from hexlet_pytest.example import reverse
+
+def test_reverse():
+    print(111111)
+    assert reverse('hexlet') == 'telxeH'
+
+# def test_reverse_for_empty_string():
+#     assert reverse('') == ''
